40ba4cb93e74657ec2897c10f91b7fe420382846
Akylbekov <user@Users-MacBook-Pro.local>
2022-05-25 21:16:36 +0600

    slot

---
+++ src/components/UI/Card.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card">
+    <span class="card-name">{{ name }}</span>
+    <span class="card-title">{{ title }} </span>
+    <div class="card-body">
+        <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      require: true,
+    },
+    title: {
+      type: String,
+      require: true,
+    },
+  }
+}
+</script>
