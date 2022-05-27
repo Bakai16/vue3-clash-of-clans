a30770d4898e828755c6fada77685e4110acfa0c
Akylbekov <user@Users-MacBook-Pro.local>
2022-05-27 12:29:51 +0600

    seeders

---
+++ src/seeders/items.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    "id": 1,
+    "title": "Archer",
+    "descr": "archer archer archer archer ",
+    "img": require('@/assets/img/archer.png'),
+    "lvl": 4
+  },
+  {
+    "id": 2,
+    "title": "Wizard",
+    "descr": " Wizard Wizard Wizard ",
+     "img": require('@/assets/img/wizard.png'),
+    "lvl": 5
+  },
+  {
+    "id": 3,
+    "title": "goblin",
+    "descr": "goblin goblin goblin" ,
+     "img": require('@/assets/img/goblin.png'),
+    "lvl": 4
+  },
+]
