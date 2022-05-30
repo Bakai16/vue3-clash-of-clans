1e08fa853823319ae3ddeddb8d5f70a90fbb50f7
Akylbekov <user@Users-MacBook-Pro.local>
2022-05-30 12:41:45 +0600

    carousel

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,41 +1,70 @@
 <template>
-  <div class="md-body">
-    <div class="card__wrapper" v-for="item in items" :key="item.id">
-      <Card
-        :name="`${item.lvl} level`"
-        :title="item.title"
-        :imgUrl="item.img"
-        :link="`/${item.alias}`"
-      >
-        <template v-slot:body>
-          {{ item.descr }}
-        </template>
-        <template v-slot:footer>
-          <div class="card-stats">
-            <div
-              v-for="(stat, index) in item.info"
-              :key="index"
-              class="one-third"
-            >
-              <div class="stat-value">{{ stat.value }}</div>
-              <div class="stat">{{ stat.title }}</div>
+    <carousel :settings="settings" :breakpoints="breakpoints">
+      <slide class="card__wrapper" v-for="item in items" :key="item.id">
+        <Card
+          :name="`${item.lvl} level`"
+          :title="item.title"
+          :imgUrl="item.img"
+          :link="`/${item.alias}`"
+        >
+          <template v-slot:body>
+            {{ item.descr }}
+          </template>
+          <template v-slot:footer>
+            <div class="card-stats">
+              <div
+                v-for="(stat, index) in item.info"
+                :key="index"
+                class="one-third"
+              >
+                <div class="stat-value">{{ stat.value }}</div>
+                <div class="stat">{{ stat.title }}</div>
+              </div>
             </div>
-          </div>
-        </template>
-      </Card>
-    </div>
-  </div>
+          </template>
+        </Card>
+      </slide>
+
+      <!-- addons -->
+      <template #addons>
+        <navigation />
+      </template>
+    </carousel>
 </template>
 
 <script>
 import items from '@/seeders/items.js'
 
 import Card from '@/components/UI/Card'
+
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Navigation } from 'vue3-carousel'
+
 export default {
-  components: { Card },
+  components: {
+    Card,
+    Carousel,
+    Slide,
+    Navigation
+  },
   data() {
     return {
-      items
+      items,
+
+      //slider
+      settings: {
+        itemsToShow: 2,
+        wrapAround: true,
+        snapAlign: 'center'
+      },
+      breakpoints: {
+        300: {
+          itemsToShow: 1
+        },
+        700: {
+          itemsToShow: 2
+        }
+      }
     }
   }
 }
